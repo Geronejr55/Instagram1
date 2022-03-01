1031fbe61335de04f2a7b0464f7bf4d0c1063901
Geronejr55 <56701309+Geronejr55@users.noreply.github.com>
2022-02-28 22:27:57 -0500

    Add files via upload

---
+++ FeedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeefViewController.swift
+//  Parstagram
+//
+//  Created by Gerone Hamilton Jr. on 2/28/22.
+//
+
+import UIKit
+
+class FeedViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Gerone Hamilton Jr. on 2/28/22.
+//
+
+import UIKit
+
+
+class LoginViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBAction func usernameField(_ sender: Any) {
+    }
+    
+    @IBAction func passwordField(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func OnSignIn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func OnSignUp(_ sender: Any) {
+    }
+    
+}
+
+
+
